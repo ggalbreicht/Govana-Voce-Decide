59aa32dc4190e8c7d2c02b60d3847c8a6c037cbf
ggalbreicht <163944744+ggalbreicht@users.noreply.github.com>
2024-05-29 22:48:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const caixaPrincipal = document.querySelector(".caixa-pincipal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+  {
+    enunciado: "Pergunta 1",
+    alternativas: [
+      "resposta 1",
+      "Resposta 2"
+    ]
+  },
+  
+  {
+    enunciado: "Pergunta 2",
+    alternativas: [
+      "resposta 1",
+      "Resposta 2"
+    ]
+  },
+
+  {
+    enunciado: "Pergunta 3",
+    alternativas: [
+      "resposta 1",
+      "Resposta 2"
+    ]
+  },
+
+  {
+    enunciado: "Pergunta 4",
+    alternativas: [
+      "resposta 1",
+      "Resposta 2"
+    ]
+  },
+
+  {
+    enunciado: "Pergunta 5",
+    alternativas: [
+      "resposta 1",
+      "Resposta 2"
+    ]
+  }
+];
